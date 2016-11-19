e34d0ef42b3ca1edaa9ebf7aacf05cbdefe8527b
Ben Flores <benjaminhyw@gmail.com>
2016-11-19 14:25:24 -0800

    Create succesful first link (to own profile once in own profile)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,15 +4,19 @@
     <title>Users</title>
   </head>
   <body>
+
+  <a href=" {{ URL::to('users') }}/{{ $user->id}}/">{{$user->fullname}}</a>
+
   <?php
-
-    echo $user->fullname . '<br>';
-
+    echo  '<br>';
+  
     foreach($user->connections as $connection){
       $friend_id = $connection->friend_id;
       //This next bit of code is another way to grab data using queries
       $friend = DB::table('users')->where('id', $friend_id)->get();
       echo '<br>' . $friend[0]->fullname  ; 
+
+
     }
   ?>
 
