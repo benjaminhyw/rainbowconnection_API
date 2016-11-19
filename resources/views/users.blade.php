81bc768502ed062bc3c1fd34ea7c8b10a2a5ccd9
Ben Flores <benjaminhyw@gmail.com>
2016-11-19 15:40:45 -0800

    Include everyone's favorite color right next to their name

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -9,7 +9,9 @@
     echo  '<br>';
     foreach($users as $user){
       $id = $user->id;
+      $favorite_color = $user->favorite_color;
       echo '<br><a href='.URL::to('/users').'/'.$id. '>'.$user->fullname.'</a>';
+      echo ' | '. $favorite_color;
     }
   ?>
   </body>
