d278348a06b968b40c6152059aff3570a1f873e1
Ben Flores <benjaminhyw@gmail.com>
2016-11-19 00:07:36 -0800

    Move function for finding all users from web.php route to UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+use App\User;
+
 class UserController extends Controller
 {
-    //
+    public function customer($id){
+      $user = User::find($id);
+      echo $user->fullname . '<br>';
+
+      foreach($user->connections as $connection){
+        $friend_id = $connection->friend_id;
+        //This next bit of code is another way to grab data using queries
+        $friend = DB::table('users')->where('id', $friend_id)->get();
+        echo '<br>' . $friend[0]->fullname  ;
+      };
 }
