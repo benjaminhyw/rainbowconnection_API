7bfb1c55ae7a310011f8d91855a30b6911dda049
Ben Flores <benjaminhyw@gmail.com>
2016-11-19 00:23:28 -0800

    Refactor UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,14 +9,8 @@ use App\User;
 
 class UserController extends Controller
 {
-    public function customer($id){
+    public function user($id){
       $user = User::find($id);
-      echo $user->fullname . '<br>';
-
-      foreach($user->connections as $connection){
-        $friend_id = $connection->friend_id;
-        //This next bit of code is another way to grab data using queries
-        $friend = DB::table('users')->where('id', $friend_id)->get();
-        echo '<br>' . $friend[0]->fullname  ;
-      };
+      return view('user', array('user' => $user));
+    }
 }
