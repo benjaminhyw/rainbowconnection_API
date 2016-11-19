f880d2d050a09678625a238c9fc68d1437eecf30
Ben Flores <benjaminhyw@gmail.com>
2016-11-19 11:16:17 -0800

    Revert "Create new route that reflects new middleware path"
    This reverts commit dd72b7a7a45ef69adbf37550943a407e93bd59c7.

--- routes/web.php
+++ routes/web.php
@@ -25,15 +25,6 @@ Route::get('users', 'UserController@users'
 Route::get('/users/{id}', 'UserController@user'
 );
 
-Route::get('/home', 'HomeController@index');
-
-
-Route::get('/admin', function(){
-  echo 'You have access';
-})->middleware('admin');
-
-
-
 Route::get('mypage', function(){
   $data = array(
     'var1' => 'Hamburger',
