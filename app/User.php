4498ebfe63a9169ebf8a23e9b1b4b71556530882
Ben Flores <benjaminhyw@gmail.com>
2016-11-19 12:09:58 -0800

    Update model and controllers to reflect fullname rather than name

--- app/User.php
+++ app/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'fullname', 'email', 'password', 'favorite_color',
+        'fullname', 'email', 'password',
     ];
 
     /**
