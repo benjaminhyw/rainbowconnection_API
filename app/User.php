b13f65fd6cc17c23191260d40acf7dab94db36fc
Ben Flores <benjaminhyw@gmail.com>
2016-11-18 21:27:51 -0800

    Modify User.php to better reflect models

--- app/User.php
+++ app/User.php
@@ -2,28 +2,35 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'favorite_color', 
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    //
 }
+
+// use Illuminate\Notifications\Notifiable;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
+
+// class User extends Authenticatable
+// {
+//     use Notifiable;
+
+//     *
+//      * The attributes that are mass assignable.
+//      *
+//      * @var array
+     
+//     protected $fillable = [
+//         'name', 'favorite_c', 
+//     ];
+
+//     /**
+//      * The attributes that should be hidden for arrays.
+//      *
+//      * @var array
+//      */
+//     protected $hidden = [
+//         'password', 'remember_token',
+//     ];
+// }
