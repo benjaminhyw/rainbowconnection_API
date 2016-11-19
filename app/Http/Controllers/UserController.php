63de53e8fafc370191e4a8b82bb9a2aca91cbe5d
Ben Flores <benjaminhyw@gmail.com>
2016-11-19 15:33:10 -0800

    Move location of users()

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,31 +10,22 @@ use App\User;
 class UserController extends Controller
 {
 
+  public function users(){
+    $users = User::all();
+    return view('users', compact('users'));
+  }
+
   //This creates a JSON file of all my users
   public function json(){
     $users = User::all();
     return $users;
-
   }
 
   public function user($id){
     $user = User::find($id);
     return view('userprofile', compact('user'));
   }
-
-  public function users(){
-    $users = User::all();
-    foreach($users as $user){
-      $id = $user->id;
-
-
-      echo $id;
-      echo $user->fullname.'<br>';
-
-
-
-    }
-  }
+  
 }
 
 
