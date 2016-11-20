598fc4fa76705d24d6f608996af69c3ddf6abdb4
Ben Flores <benjaminhyw@gmail.com>
2016-11-19 17:15:19 -0800

    Include jquery library

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -2,12 +2,17 @@
 <html>
   <head>
     <title>Users</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
   </head>
   <body>
   
   <br>
     @foreach($users as $user)
-      <br><a href="/users/{{$user->id}}">{{$user->fullname}}</a>  |  {{$user->favorite_color}};
+    <div>
+      <h3><a href="/users/{{$user->id}}">{{$user->fullname}}</a>  |  {{$user->favorite_color}}</h3>
+    </div>
     @endforeach
   </body>
 </html>
+
+
