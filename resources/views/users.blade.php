c7e794a949edbf959d9de6a4d48f71b50fd3714a
Ben Flores <benjaminhyw@gmail.com>
2016-11-19 16:53:54 -0800

    Refactor users.blade.php for blade syntax

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -4,15 +4,10 @@
     <title>Users</title>
   </head>
   <body>
-
-  <?php
-    echo  '<br>';
-    foreach($users as $user){
-      $id = $user->id;
-      $favorite_color = $user->favorite_color;
-      echo '<br><a href='.URL::to('/users').'/'.$id. '>'.$user->fullname.'</a>';
-      echo ' | '. $favorite_color;
-    }
-  ?>
+  
+  <br>
+    @foreach($users as $user)
+      <br><a href="/users/{{$user->id}}">{{$user->fullname}}</a>  |  {{$user->favorite_color}};
+    @endforeach
   </body>
 </html>
